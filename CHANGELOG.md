8a8bc2d019cbd1f7de8628c740e6a6888f91079e
Constant-Contact-Public-API <ctct-publicapi@constantcontact.com>
2025-05-13 09:25:40 -0400

    1.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/) and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## 1.3.0 - 2025-05-13
--- src/ConstantContactApi/Model/CreateOrUpdateContactRequestSmsChannel.cs
+++ src/ConstantContactApi/Model/CreateOrUpdateContactRequestSmsChannel.cs
@@ -29,45 +29,45 @@ namespace ConstantContactApi.Model
     /// <summary>
     /// The contact&#39;s SMS details.
     /// </summary>
-    [DataContract(Name = "JmmlSmsChannel")]
-    public partial class JmmlSmsChannel : IValidatableObject
+    [DataContract(Name = "createOrUpdateContact_request_sms_channel")]
+    public partial class CreateOrUpdateContactRequestSmsChannel : IValidatableObject
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="JmmlSmsChannel" /> class.
+        /// Initializes a new instance of the <see cref="CreateOrUpdateContactRequestSmsChannel" /> class.
         /// </summary>
         [JsonConstructorAttribute]
-        protected JmmlSmsChannel() { }
+        protected CreateOrUpdateContactRequestSmsChannel() { }
         /// <summary>
-        /// Initializes a new instance of the <see cref="JmmlSmsChannel" /> class.
+        /// Initializes a new instance of the <see cref="CreateOrUpdateContactRequestSmsChannel" /> class.
         /// </summary>
         /// <param name="smsAddress">The contact&#39;s SMS-capable phone number, excluding the country code. (required).</param>
         /// <param name="dialCode">The dial code the country uses. For example, use &lt;code&gt;1&lt;/code&gt; for the United States dial code. (required).</param>
         /// <param name="countryCode">The two-digit code that identifies the country. (required).</param>
         /// <param name="smsChannelConsents">The consents provided for the SMS Channel. (required).</param>
-        public JmmlSmsChannel(string smsAddress = default(string), string dialCode = default(string), string countryCode = default(string), List<CreateOrUpdateContactRequestSmsChannelSmsChannelConsentsInner> smsChannelConsents = default(List<CreateOrUpdateContactRequestSmsChannelSmsChannelConsentsInner>))
+        public CreateOrUpdateContactRequestSmsChannel(string smsAddress = default(string), string dialCode = default(string), string countryCode = default(string), List<CreateOrUpdateContactRequestSmsChannelSmsChannelConsentsInner> smsChannelConsents = default(List<CreateOrUpdateContactRequestSmsChannelSmsChannelConsentsInner>))
         {
             // to ensure "smsAddress" is required (not null)
             if (smsAddress == null)
             {
-                throw new ArgumentNullException("smsAddress is a required property for JmmlSmsChannel and cannot be null");
+                throw new ArgumentNullException("smsAddress is a required property for CreateOrUpdateContactRequestSmsChannel and cannot be null");
             }
             this.SmsAddress = smsAddress;
             // to ensure "dialCode" is required (not null)
             if (dialCode == null)
             {
-                throw new ArgumentNullException("dialCode is a required property for JmmlSmsChannel and cannot be null");
+                throw new ArgumentNullException("dialCode is a required property for CreateOrUpdateContactRequestSmsChannel and cannot be null");
             }
             this.DialCode = dialCode;
             // to ensure "countryCode" is required (not null)
             if (countryCode == null)
             {
-                throw new ArgumentNullException("countryCode is a required property for JmmlSmsChannel and cannot be null");
+                throw new ArgumentNullException("countryCode is a required property for CreateOrUpdateContactRequestSmsChannel and cannot be null");
             }
             this.CountryCode = countryCode;
             // to ensure "smsChannelConsents" is required (not null)
             if (smsChannelConsents == null)
             {
-                throw new ArgumentNullException("smsChannelConsents is a required property for JmmlSmsChannel and cannot be null");
+                throw new ArgumentNullException("smsChannelConsents is a required property for CreateOrUpdateContactRequestSmsChannel and cannot be null");
             }
             this.SmsChannelConsents = smsChannelConsents;
         }
@@ -110,7 +110,7 @@ namespace ConstantContactApi.Model
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("class JmmlSmsChannel {\n");
+            sb.Append("class CreateOrUpdateContactRequestSmsChannel {\n");
             sb.Append("  SmsAddress: ").Append(SmsAddress).Append("\n");
             sb.Append("  DialCode: ").Append(DialCode).Append("\n");
             sb.Append("  CountryCode: ").Append(CountryCode).Append("\n");
